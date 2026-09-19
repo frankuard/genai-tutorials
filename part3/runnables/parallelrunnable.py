17ff39e9515296187ff1114733c2c8476f8e4825
Roshan Karki <roshankarki4956@gmail.com>
2026-09-19 16:54:57 +0545

    learning about runnable lambda

--- part3/runnables/parallelrunnable.py
+++ part3/runnables/parallelrunnable.py
@@ -5,7 +5,7 @@ load_dotenv()
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel, RunnableLambda
 
 
 ## COMPONENTS
@@ -31,11 +31,14 @@ detailed_prompt = ChatPromptTemplate.from_template(
 topic = "Machine Learning"
 
 chain = RunnableParallel({
-    "short" : short_prompt | model | parser,
-    "detailed" : detailed_prompt | model | parser,
+    "short" : RunnableLambda(lambda x:x ['short']) |short_prompt | model | parser,
+    "detailed" :RunnableLambda(lambda x:x ['detailed']) | detailed_prompt | model | parser,
 })
 
-result = chain.invoke({'topic': "Machine Learning"})
+result = chain.invoke({
+    "short":{'topic': "Machine Learning"},
+    "detailed":{'topic': "Deep Learning"}
+})
 
 print(result['short'])
 print(result['detailed'])
