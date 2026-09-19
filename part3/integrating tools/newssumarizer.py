0a5dc7cafc00a014058d7cf0a5cfc8ac1b4185c8
Roshan Karki <roshankarki4956@gmail.com>
2026-09-20 00:16:57 +0545

    learning about search tools

---
+++ part3/integrating tools/newssumarizer.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+search_tool = TavilySearchResults(max_result= 5)
+
+llm = ChatGroq(
+    model="openai/gpt-oss-120b"
+)
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    You are a helpful assistant 
+    Summarize the following news into clear bull 
+    
+    {news}
+    """
+)
+
+chain = prompt | llm | StrOutputParser()
+
+news_result = search_tool.run("Latest AI news of 2026")
+
+result = chain.invoke({"news": news_result})
+
+print(result)
